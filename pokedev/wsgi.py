eacc0118e2111885964783f91ce2b10c75a34b73
sebschopf <seba.schopfer@gmail.com>
2025-05-19 14:41:05 +0200

    Fix deployment issues: add output.css, fix wsgi.py, update requirements

--- pokedev/wsgi.py
+++ pokedev/wsgi.py
@@ -6,7 +6,7 @@ from django.core.wsgi import get_wsgi_application
 from dotenv import load_dotenv
 
 # Chargement des variables d'environnement depuis le fichier .env
-load_dotenv
+load_dotenv()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pokedev.settings')
 
