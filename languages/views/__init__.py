9b1272e4648f5d3548bdacd82334d973738aa636
sebschopf <seba.schopfer@gmail.com>
2025-05-22 16:55:22 +0200

    Correction de l'importation framework_detail et ajout des migrations pour les modèles d'accessibilité et de fonctionnalités

--- languages/views/__init__.py
+++ languages/views/__init__.py
@@ -1,3 +1,3 @@
 # Import des vues principales pour les rendre disponibles directement depuis languages.views
-from .language_views import LanguageDetailView, list, framework_detail
-
+from .language_views import LanguageDetailView, list
+from .framework_views import framework_detail  # Importation depuis le bon fichier
